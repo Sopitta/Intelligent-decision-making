8ed17573cab865b6cab8c0437f7a6a0bae0daf44
Thurachen Sopitta <sopitta.thurachen@aalto.fi>
2021-03-15 00:24:07 +0200

    added Global planner/more methods in the agent

--- agent/myagent.py
+++ agent/myagent.py
@@ -18,12 +18,15 @@ sys.path.insert(1, 'D:/Master thesis/agent/navigation')
 #sys.path.insert(0, '..')
 #from navigation.waypoint_gen import WaypointGen
 from waypoint_gen import WaypointGen
+from global_route_planner import GlobalRoutePlanner
+from global_route_planner_dao import GlobalRoutePlannerDAO
 
 class Agent:
     def __init__(self, vehicle, target_speed = 20):
          self.vehicle = vehicle
          self.waypoint_gen = WaypointGen(self.vehicle, opt_dict={'target_speed' : target_speed}) 
-         self_action = None
+         self._hop_resolution = 2.0
+         self.action = None
     def safeaction(self,distance):
         if distance < 5:
             self.action = 1 #change
@@ -38,9 +41,30 @@ class Agent:
         next_wp = self.waypoint_gen._compute_next_waypoints()
         return next_wp 
     '''
+    def _trace_route(self, start_waypoint, end_waypoint):
+        """
+        This method sets up a global router and returns the optimal route
+        from start_waypoint to end_waypoint
+        """
+
+        # Setting up global router
+        if self._grp is None:
+            dao = GlobalRoutePlannerDAO(self._vehicle.get_world().get_map(), self._hop_resolution)
+            grp = GlobalRoutePlanner(dao)
+            grp.setup()
+            self._grp = grp
+
+        # Obtain route plan
+        route = self._grp.trace_route(
+            start_waypoint.transform.location,
+            end_waypoint.transform.location)
+
+        return route
+    
     def run_step(self,action):
         control = self.waypoint_gen.run_step(action)
         return control
+    
     
         
         
